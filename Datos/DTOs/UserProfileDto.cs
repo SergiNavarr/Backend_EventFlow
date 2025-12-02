723a2fb3cf8f47ee0c335f93e7af1915e71ca825
Sergio Navarro <nachonavarro197@gmail.com>
2025-12-02 16:39:27 -0300

    Creacion de DTOs para User

---
+++ Datos/DTOs/UserProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DTOs
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; } 
+        public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int FollowersCount { get; set; } // Cuánta gente me sigue
+        public int FollowingCount { get; set; } // A cuánta gente sigo
+    }
+}
